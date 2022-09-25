ccdb41f4d34774ade0a7506ca3666d876fb5bde3
ChantikaTriAmanda <70676084+ChantikaTriAmanda@users.noreply.github.com>
2022-09-25 17:29:26 +0700

    Mengupload Tugas Minggu Pertama

---
+++ Chantika120922/helloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chantika120922;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+        System.out.println(" Pemrograman Berorientasi Objek");
+    }
+}
