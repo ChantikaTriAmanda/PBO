41712e6f715c73feb3c3fcf9665c859376fa94c1
ChantikaTriAmanda <70676084+ChantikaTriAmanda@users.noreply.github.com>
2023-01-05 11:29:02 +0700

    Mengupload Folder Teori
    Mengupload Latihan - Latihan Teori dari modul

---
+++ Teori/ChantikaModul3/ChantikaTriAmanda.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChantikaModul3;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ChantikaTriAmanda {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming Chantika Tri Amanda !!!");
+    }
+}
